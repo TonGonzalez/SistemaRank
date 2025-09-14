f6ec6f6903bd0f20aa97c4a34a15daad9fd693ed
TonGonzalez <ayrtongonzalez1@gmail.com>
2025-09-13 22:35:46 -0300

    First Commit

---
+++ src/main/java/com/rank/SistemaRank/SistemaRankApplication.java
@@ -0,0 +1,13 @@
+package com.rank.SistemaRank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaRankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaRankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rank/SistemaRank/SistemaRankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rank.SistemaRank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaRankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
