dd036fc1615cc535de64703a6583b11b74cd1fb9
TonGonzalez <ayrtongonzalez1@gmail.com>
2025-09-24 11:15:58 -0300

    feat: creates the interface for the Missions layer

---
+++ src/main/java/com/rank/SistemaRank/Missions/MissionsRepository.java
@@ -0,0 +1,6 @@
+package com.rank.SistemaRank.Missions;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MissionsRepository extends JpaRepository<MissionsModel, Long> {
+}
